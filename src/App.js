4e103f6b522403081855bfc3c0d8e1d045edfc69
ArturCandelori <artur.candelori@hotmail.com>
2021-01-14 12:22:57 -0300

    criando rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route exact path='/'>
+          hello
+        </Route>
+        <Route path='/product/list'>product list</Route>
+        <Route path='/product/save'>new product</Route>
+        <Route path='/login' component={LoginForm} />
+      </Switch>
     </div>
   );
 }
